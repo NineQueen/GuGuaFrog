5b47d7b2934e369d43f079f03a34b55285bea691
NineQueen <54728702+NineQueen@users.noreply.github.com>
2022-05-20 08:38:47 +0800

    Add files via upload

--- repeater.cpp
+++ repeater.cpp
@@ -1,48 +1,51 @@
 #include<iostream>
 #include<windows.h>
 #include<ctime>
+#include<string>
 #include<cstdlib>
 using namespace std;
 void fun(int a){
 	if(a==0){
-        Sleep(300);
+        Sleep(100);
         return;
     } 
 	keybd_event(17,0,0,0);
 	keybd_event(86,0,0,0);
 	keybd_event(17,0,KEYEVENTF_KEYUP,0);
 	keybd_event(86,0,KEYEVENTF_KEYUP,0); 
-	Sleep(10);
+    Sleep(5);
 	fun(a-1);
 }
 int main(){
+    cout << "Made By NineQueen"<<endl;
 	srand(time(0));
 	while(true){
 		int times;
 		cin >> times; 
-	for(int i=1;i<=4;i++){
-		cout << i;
-		Sleep(1000); 
-	}
-	keybd_event(17,0,0,0);
-	keybd_event(65,0,0,0);
-	keybd_event(17,0,KEYEVENTF_KEYUP,0);
-	keybd_event(65,0,KEYEVENTF_KEYUP,0); 
-	keybd_event(17,0,0,0);
-	keybd_event(67,0,0,0);
-	keybd_event(17,0,KEYEVENTF_KEYUP,0);
-	keybd_event(67,0,KEYEVENTF_KEYUP,0); 
-	for(int i = 1;i<=times;i++){
-		if(i%40==0){
-			Sleep(10000);
-		}
-		int rands = rand()%20+1;
-		cout << rands;
-		fun(rands);
-		keybd_event(13,0,0,0); 
-		keybd_event(13,0,KEYEVENTF_KEYUP,0);  
-	}
-	cout<<"finish"; 
-	} 
+        cout << 5<<' ';
+        for(int i=5;i>=1;i--){
+            Sleep(1000); 
+            cout << i<<' ';
+        }
+        keybd_event(17,0,0,0);
+        keybd_event(65,0,0,0);
+        keybd_event(17,0,KEYEVENTF_KEYUP,0);
+        keybd_event(65,0,KEYEVENTF_KEYUP,0); 
+        keybd_event(17,0,0,0);
+        keybd_event(67,0,0,0);
+        keybd_event(17,0,KEYEVENTF_KEYUP,0);
+        keybd_event(67,0,KEYEVENTF_KEYUP,0); 
+        for(int i = 1;i<=times;i++){
+            if(i%40==0){
+                Sleep(10000);
+            }
+            int rands = rand()%20+1;
+            cout << rands<<' ';
+            fun(rands);
+            keybd_event(13,0,0,0); 
+            keybd_event(13,0,KEYEVENTF_KEYUP,0);  
+        }
+        cout<<"finish"; 
+    } 
 	
 } 
